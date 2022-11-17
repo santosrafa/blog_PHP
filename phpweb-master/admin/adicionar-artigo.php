08af481f26e837eff71ce57aa0ea7f8496214330
TI GIIRO <ti.rafaelteles@gmail.com>
2022-11-16 21:39:57 -0300

    adicionar_artigo

--- phpweb-master/admin/adicionar-artigo.php
+++ phpweb-master/admin/adicionar-artigo.php
@@ -1,3 +1,15 @@
+<?php
+
+require '../config.php';
+require '../src/Artigo.php';
+
+if ($_SERVER ['REQUEST_METHOD'] === 'POST'){
+    $artigp = new Artigo($mysql);
+    $artigp->adicionar($_POST['titulo'], $_POST['conteudo']);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,7 +22,7 @@
 <body>
     <div id="container">
         <h1>Adicionar Artigo</h1>
-        <form action="adicionar-artigo.html" method="post">
+        <form action="adicionar-artigo.php" method="post">
             <p>
                 <label for="">Digite o título do artigo</label>
                 <input class="campo-form" type="text" name="titulo" id="titulo" />
--- phpweb-master/src/Artigo.php
+++ phpweb-master/src/Artigo.php
@@ -10,6 +10,13 @@
             
         }
 
+        public function adicionar (string $titulo, string $conteudo): void
+        {
+            $insereArtigo = $this->mysql->prepare('INSERT INTO artigos (titulo, conteudo) VALUES (?,?);');
+            $insereArtigo->bind_param('ss', $titulo, $conteudo);
+            $insereArtigo->execute();
+        }
+
         public function exibirTodos(): array
         {
             /* --query: consulta no banco-- */
